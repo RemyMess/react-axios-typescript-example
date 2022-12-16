e38b5005f0da30ac856d0e19f0be697b091a9e15
remymess <remy.messadene@gmail.com>
2022-12-16 12:55:30 +0000

    copy

--- src/Views/LandingPage.tsx
+++ src/Views/LandingPage.tsx
@@ -128,7 +128,7 @@ export default class LandingPage extends Component<Props, State> {
             }
           >
             <Text color="black" margin={{top: "40px", bottom: "15px"}} weight="bold" size="32px">
-              We make 90% of you stick with your 2023 resolutions for 2 months.
+              Make yourself 91% more likely to stick with your 2023 resolutions for 2 months.
             </Text>
           </Box>
           
